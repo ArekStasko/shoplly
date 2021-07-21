89b7f772566d0cde930f57441293b3bf83651a6d
ArekStasko <arek.stasko@gmail.com>
2021-07-21 22:23:29 +0200

    add animation to login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { loginText } from "../data/login_slides";
+import gsap from 'gsap'
 
 class Login extends React.Component {
   constructor() {
@@ -7,21 +8,31 @@ class Login extends React.Component {
     this.state = {
       textCount: 0,
     };
+    this.wrapperRef = React.createRef(null);
   }
 
+  slideTimer = () => setInterval(() => {
+    if (this.state.textCount === loginText.length - 1) {
+      this.setState({ textCount: 0 });
+    } else {
+      this.setState({ textCount: this.state.textCount + 1 });
+    }
+  }, 3000);
+
   componentDidMount() {
-    this.slideTimer = setInterval(() => {
-      if (this.state.textCount === loginText.length - 1) {
-        this.setState({ textCount: 0 });
-      } else {
-        this.setState({ textCount: this.state.textCount + 1 });
-      }
-    }, 3000);
+  this.slideTimer()
+
+  const [description, login] = this.wrapperRef.current.children
+  gsap.set([description, login], {autoAlpha: 0})
+  const tl = gsap.timeline({defaults: {ease: 'power3.inOut'}})
+
+  tl.fromTo(description, {x: '-=300'}, {duration:1, x: '+=300', autoAlpha: 1})
+    .fromTo(login, {x: '-=200'}, {duration:1.1, x: '+=200', autoAlpha: 1})
   }
 
   render() {
     return (
-      <div className="login">
+      <div ref={this.wrapperRef} className="login">
         <div className="login__description">
           <div className="login__description--slide">
             <div>
@@ -42,7 +53,7 @@ class Login extends React.Component {
             <label for="password-login">Password</label>
             <input id="password-login" type="password" />
             <button>Sign in</button>
-          </form>
+          </form> 
           <div className="login__form--register">
             <p>Don't have account ?</p>
             <button>Register</button>
