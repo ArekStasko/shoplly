f5192a98f9048fdc9fe86abfe3d95d4090d1e864
ArekStasko <arek.stasko@gmail.com>
2021-07-25 00:03:04 +0200

    add mobile styles to navbar without animations

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faPlusCircle
-} from '@fortawesome/free-solid-svg-icons'
+  faPlusCircle,
+  faTimes,
+  faEllipsisH,
+} from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+
+const NavLinks = styled.div`
+@media (max-width: 768px){
+  display: ${({ show }) => (show ? "flex" : "none")};
+}
+`;
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="navbar">
       <div className="navbar__title">
-        <Link to='/'>Shoplly</Link>
+        <Link to="/">Shoplly</Link>
       </div>
-      <div className="navbar__links">
-        <Link to='/products' >Products</Link>
-        <Link to='/login' >Login</Link>
-        <Link to='/register' >Register</Link>
-        <Link to='/products/add' >
-        <FontAwesomeIcon className='navbar__links--add' icon={faPlusCircle} />
+      <div onClick={() => setShow(!show)} className="navbar__mobileIcon">
+        {show ? (
+          <FontAwesomeIcon
+            className="navbar__mobileIcon--nonActive"
+            icon={faTimes}
+          />
+        ) : (
+          <FontAwesomeIcon
+            className="navbar__mobileIcon--active"
+            icon={faEllipsisH}
+          />
+        )}
+      </div>
+      <NavLinks
+        onClick={() => setShow(!show)}
+        show={show}
+        className="navbar__links"
+      >
+        <Link className="navbar__links--link" to="/products">
+          Products
         </Link>
-      </div>
+        <Link className="navbar__links--link" to="/login">
+          Login
+        </Link>
+        <Link className="navbar__links--link" to="/register">
+          Register
+        </Link>
+        <Link to="/products/add">
+          <FontAwesomeIcon className="navbar__links--add" icon={faPlusCircle} />
+        </Link>
+      </NavLinks>
     </div>
   );
 };
