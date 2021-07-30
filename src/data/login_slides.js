cf3dc1b8925d8aa2bafc838b7e60a0ddf3b2c093
ArekStasko <arek.stasko@gmail.com>
2021-07-30 23:28:59 +0200

    add basic select logic to contact form

--- src/data/login_slides.js
+++ src/data/login_slides.js
@@ -1,25 +1,55 @@
-import Buy from '../assets/images/login_slides/buy.svg'
-import Sell from '../assets/images/login_slides/sell.svg'
-import Rate from '../assets/images/login_slides/rate.svg'
-
+import Buy from "../assets/images/login_slides/buy.svg";
+import Sell from "../assets/images/login_slides/sell.svg";
+import Rate from "../assets/images/login_slides/rate.svg";
 
 export const loginText = [
-    {
-      title: "Buy cheap and fast",
-      description:
-        "After purchasing the premium version, the parcel will always be delivered on the next day!",
-      icon: Buy,  
-    },
-    {
-      title: "Sell everything you want",
-      description:
-        "In our shops you can even sell your used socks",
-      icon: Sell,  
-    },
-    {
-      title: "You can rate products",
-      description:
-        "you can leave a review or comment",
-      icon: Rate,  
-    },
-  ];
+  {
+    title: "Buy cheap and fast",
+    description:
+      "After purchasing the premium version, the parcel will always be delivered on the next day!",
+    icon: Buy,
+  },
+  {
+    title: "Sell everything you want",
+    description: "In our shops you can even sell your used socks",
+    icon: Sell,
+  },
+  {
+    title: "You can rate products",
+    description: "you can leave a review or comment",
+    icon: Rate,
+  },
+];
+
+export const RegisterAdressData = [
+  {
+    Province: ["First select Province"],
+    Dolnoslaskie: ["Jelenia Gora", "Legnica", "Walbrzych"],
+    "Kujawsko-pomorskie": ["Bydgoszcz", "Grudziądz", "Torun", "Wloclawek"],
+    Lubelskie: ["Biala Podlaska", "Chelm", "Lublin", "Zamosc"],
+    Lubuskie: ["Gorzow Wielkopolski", "Zielona Gora"],
+    Lodzkie: ["Lodz", "Piotrkow Trybunalski", "Skierniewice"],
+    Malopolskie: ["Krakow", "Nowy Sacz", "Tarnow"],
+    Mazowieckie: ["Ostroleka", "Plock", "Radom", "Siedlce", "Warszawa"],
+    Opolskie: ["Opole"],
+    Podkarpackie: ["Krosno", "Przemysl", "Rzeszow", "Tarnobrzeg"],
+    Podlaskie: ["Bialystok", "Lomza", "Suwalki"],
+    Pomorskie: ["Gdansk", "Gdynia", "Slupsk", "Sopot"],
+    Slaskie: [
+      "Zory",
+      "Bytom",
+      "Bielsko-biala",
+      "Chorzow",
+      "Czestochowa",
+      "Dabrowa Gornicza",
+      "Gliwice",
+      "Jastrzebie-Zdroj",
+      "Jaworzno",
+      "Katowice",
+    ],
+    Swietokrzyskie: ["Kielce"],
+    "Warminsko-mazurskie": ["Elblag", "Olsztyn"],
+    Wielkopolskie: ["Kalisz", "Konin", "Leszno", "Poznan"],
+    Zachodniopomorskie: ["Koszalin", "Swinioujscie", "Szczecin"],
+  },
+];
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { RegisterAdressData } from "../data/login_slides";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faArrowRight,
@@ -13,9 +14,26 @@ class Register extends React.Component {
       registerStep: 0,
       password: "",
       nickname: "",
-      contact: {},
+      contact: {
+        number: "",
+        email: "",
+        province: "Province",
+        city: "",
+      },
     };
   }
+
+  provinceSelect = (e) => {
+    this.setState({
+      contact: { ...this.state.contact, province: e.target.value },
+    });
+  };
+
+  citySelect = (e) => {
+    this.setState({
+      contact: { ...this.state.contact, city: e.target.value },
+    });
+  };
 
   render() {
     return (
@@ -23,18 +41,45 @@ class Register extends React.Component {
         {this.state.registerStep === 1 ? (
           <div className="register__contact-data">
             <form className="register__contact-data--form">
-              <label htmlFor="register-number">Your number</label>
-              <input id="register-number" type="text" />
-              <label htmlFor="register-email">Your email</label>
-              <input id="register-email" type="text" />
+              <div>
+                <label htmlFor="register-number">Your number</label>
+                <input id="register-number" type="text" />
+              </div>
+              <div>
+                <label htmlFor="register-email">Your email</label>
+                <input id="register-email" type="text" />
+              </div>
               <div className="register__contact-data--form--place">
                 <div>
-                  <label htmlFor="register-adress">Choose your province</label>
-                  <input id="register-adress" type="text" />
+                  <label htmlFor="register_adress-province">
+                    Choose your province
+                  </label>
+                  <select
+                    onChange={(e) => this.provinceSelect(e)}
+                    id="register_adress-province"
+                  >
+                    <option value="Province">Province</option>
+                    {Object.entries(RegisterAdressData[0]).map((item) => (
+                      <option key={item[0]} value={item[0]}>
+                        {item[0]}
+                      </option>
+                    ))}
+                  </select>
                 </div>
                 <div>
-                  <label htmlFor="register-adress">Choose your city</label>
-                  <input id="register-adress" type="text" />
+                  <label htmlFor="register_adress-city">Choose your city</label>
+                  <select
+                    onChange={(e) => this.citySelect(e)}
+                    id="register_adress-city"
+                  >
+                    {RegisterAdressData[0][this.state.contact.province].map(
+                      (item) => (
+                        <option key={item} value={item}>
+                          {item}
+                        </option>
+                      )
+                    )}
+                  </select>
                 </div>
               </div>
               <button type="submit">
